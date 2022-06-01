c74d8e52c5cb9bdf27faacb62bbfcd9dd3b82186
istareatotherscode <91970414+istareatotherscode@users.noreply.github.com>
2022-06-01 10:23:50 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -86,11 +86,6 @@ aria2 = ariaAPI(
 def get_client():
     return qbClient(host="localhost", port=8090)
 
-trackers = check_output(["curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all | awk '$0'"], shell=True).decode('utf-8')
-trackerslist = set(trackers.split("\n"))
-trackerslist.remove("")
-trackerslist = "\n\n".join(trackerslist)
-get_client().application.set_preferences({"add_trackers": f"{trackerslist}"})
 
 DOWNLOAD_DIR = None
 BOT_TOKEN = None
@@ -491,13 +486,6 @@ if ospath.exists('drive_folder'):
                 INDEX_URLS.append(temp[2])
             except:
                 INDEX_URLS.append(None)
-try:
-    SEARCH_PLUGINS = getConfig('SEARCH_PLUGINS')
-    if len(SEARCH_PLUGINS) == 0:
-        raise KeyError
-    SEARCH_PLUGINS = jsnloads(SEARCH_PLUGINS)
-except:
-    SEARCH_PLUGINS = None
 
 updater = tgUpdater(token=BOT_TOKEN, request_kwargs={'read_timeout': 20, 'connect_timeout': 15})
 bot = updater.bot
